a20eaecb51dce4bbb9e35a3b9d3f4bdfbe1a1e77
Daniel Tisue <d.tisue@gmail.com>
2019-04-08 13:28:53 +0000

    add auth routes and userSchema

--- app.js
+++ app.js
@@ -2,24 +2,45 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const methodOverride = require("method-override");
 const Skatespot = require("./models/skatespot");
 const Comment = require("./models/comment");
-//const User = require("./models/user");
-const methodOverride = require("method-override");
+const User = require("./models/user");
 const seedDB = require("./seeds");
+
+//require routes
 const commentRoutes = require("./routes/comments");
 const skatespotRoutes = require("./routes/skatespots");
+const authRoutes      = require("./routes/auth");
 
 mongoose.connect("mongodb://localhost:27017/slam_spot", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
-
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 seedDB();
 
-// app.use("/", authRoutes);
+//Passport Config
+app.use(require("express-session")({
+    secret: "Max is feeling better!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
+app.use("/", authRoutes);
 app.use("/skateSpots", skatespotRoutes);
 app.use("/skateSpots/:id/comments", commentRoutes);
 
@@ -27,61 +48,6 @@ app.get('/', (req, res) => {
  res.render('landing'); 
 });
 
-// //INDEX
-// app.get('/skateSpots', (req,res) => {
-//     Skatespot.find({}, (err, allSkatespots) => {
-//     if(err){
-//         console.log(err);
-//     } else {
-//         res.render('skatespots/index', {skatespots: allSkatespots});
-//     }
-//   });
-// });
-
-// //CREATE Skatespot
-// app.post("/skateSpots", (req, res) => {
-//     // get data from form and add to skatespots array
-//     var name = req.body.name;
-//     var image = req.body.image;
-//     var desc = req.body.description;
-//     var newSkatespot = {name: name, image: image, description: desc};
-//     // Create a new skatespot and save to DB
-//     Skatespot.create(newSkatespot, function(err, newlyCreated){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             //redirect back to skatespots page
-//             res.redirect("skatespots/index");
-//         }
-//     });
-// });
-
-// //NEW - show form to create new skatespot
-// app.get("/skateSpots/new", (req, res) => {
-//   res.render("skatespots/new"); 
-// });
-
-// //SHOW page
-// app.get("/skateSpots/:id", function(req, res) {
-//     //find the skatespot with provided ID
-//     // Skatespot.findById(req.params.id, function(err, foundSkatespot){
-//     //     if(err){
-//     //         console.log(err);
-//     //     } else {
-//     //         res.render("show", {skatespot: foundSkatespot});
-//     //     }
-//     // });
-    
-//     Skatespot.findById(req.params.id).populate("comments").exec(function(err, foundSkatespot){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             console.log(foundSkatespot);
-//             //render show template with that skatespot
-//             res.render("skatespots/show", {skatespot: foundSkatespot});
-//         }
-//     });
-// });
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started!");
--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
-//var passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
     username: String, 
     password: String,
-    firstName: String,
-    lastName: String,
-    email: String,
+    // firstName: String,
+    // lastName: String,
+    // email: String,
     //isAdmin: {type: Boolean, default: false}
 });
 
-//UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
