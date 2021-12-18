1c0332aca899c0ddebb13309f2dcc17f8a4bb161
DanielTisue <d.tisue@gmail.com>
2021-12-18 08:41:04 -0600

    Refactored schema with auth packages update.

--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
-const mongoose = require("mongoose"),
-      passportLocalMongoose =   require("passport-local-mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  password: String,
-  email: { type: String, unique: true, required: true },
-  resetPasswordToken: String,
-  resetPasswordExpires: Date
+const UserSchema = new Schema({
+  // username: { type: String, unique: true, required: true },
+  // password: String,
+  email: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  }
+  // resetPasswordToken: String,
+  // resetPasswordExpires: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
