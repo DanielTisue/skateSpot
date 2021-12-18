2a2d1137fec217f11abc012b4447f2f36f8dc661
DanielTisue <d.tisue@gmail.com>
2021-12-18 08:45:11 -0600

    Added app.use(express.urlencoded({ extended: true })); to coincide with updated express form processing requirements.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require("dotenv").config({ path: '.env' });
 const express = require("express"),
-  app = express(),
-  mongoose = require("mongoose"),
-  flash = require("connect-flash"),
-  passport = require("passport"),
-  LocalStrategy = require("passport-local"),
-  methodOverride = require("method-override"),
-  Skatespot = require("./models/skatespot"),
-  Comment = require("./models/comment"),
-  User = require("./models/user");
+      app = express(),
+      mongoose = require("mongoose"),
+      flash = require("connect-flash"),
+      passport = require("passport"),
+      LocalStrategy = require("passport-local"),
+      methodOverride = require("method-override"),
+      Skatespot = require("./models/skatespot"),
+      Comment = require("./models/comment"),
+      User = require("./models/user");
 
 
 //require routes
@@ -17,17 +17,19 @@ const commentRoutes = require("./routes/comments"),
   authRoutes = require("./routes/auth");
 
 //db config
-mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log('Database is connected!');
-})
+   console.log('Database is connected!');
+});
 
-app.use(express.json({ extended: true }));
+// app.use(express.json({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
+
+app.use(express.urlencoded({ extended: true }));
 
 //Passport Config
 app.use(
@@ -40,10 +42,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
@@ -58,6 +61,6 @@ app.get("/", (req, res) => {
   res.render("landing");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 4000, () => {
    console.log("Server Started!");
  });
