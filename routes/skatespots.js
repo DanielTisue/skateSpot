62e2cabc082a70a8b62fc47790338b4e5151cf5f
DanielTisue <d.tisue@gmail.com>
2022-01-04 11:47:25 -0600

    Added image upload function.

--- routes/skatespots.js
+++ routes/skatespots.js
@@ -2,18 +2,20 @@ const express = require('express'),
       router = express.Router({ mergeParams: true }),
       skatespots = require('../controllers/skatespots'),
       catchAsync = require('../utils/catchAsync'),
-      { isLoggedIn, validateSkatespot, isAuthor } = require('../middleware/index');
-
-      const multer = require('multer'),
-            upload = multer({dest: 'uploads/'});
+      { isLoggedIn, validateSkatespot, isAuthor } = require('../middleware/index'),
+      multer = require('multer'),
+      { storage } = require('../cloudinary');
+            
       // Skatespot = require('../models/skatespot');
      
+const upload = multer({ storage });
+
 
 //INDEX - CREATE
 router.route('/')
       .get(catchAsync(skatespots.index))
-      .post(isLoggedIn, validateSkatespot, catchAsync(skatespots.createSkatespot));
-      
+      .post(isLoggedIn, upload.array('image'), validateSkatespot, catchAsync(skatespots.createSkatespot));
+     
 
 
 //NEW form
@@ -22,7 +24,7 @@ router.get('/new', isLoggedIn, skatespots.renderNewForm);
 //SHOW, UPDATE, DELETE
 router.route('/:id')
       .get(catchAsync(skatespots.showSkatespot))
-      .put(isLoggedIn, isAuthor, validateSkatespot, catchAsync(skatespots.updateSkatespot))
+      .put(isLoggedIn, isAuthor, upload.array('image'), validateSkatespot, catchAsync(skatespots.updateSkatespot))
       .delete(isLoggedIn, isAuthor, catchAsync(skatespots.deleteSkatespot));
 
 // EDIT form
