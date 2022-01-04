cb8f999e9e9730d246612a7d28ff0f8ed74ffa5e
DanielTisue <d.tisue@gmail.com>
2022-01-04 11:45:14 -0600

    Added delete method for cloudinary image array. Files can now be deleted from both DB and cloudinary when skatespot is deleted.

--- models/skatespot.js
+++ models/skatespot.js
@@ -1,10 +1,23 @@
+const { cloudinary } = require('../cloudinary');
+
 const mongoose = require('mongoose'),
       Comment = require('./comment'),
       Schema = mongoose.Schema;
 
+      // https://res.cloudinary.com/danieltisue/image/upload/v1641269692/skateSpots/uk3ndntebdgze6udpd0c.png
+
+const ImageSchema = new Schema({
+      url: String,
+      filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+  return this.url.replace('/upload', '/upload/w_500');
+});
+
 const skatespotSchema = new Schema({
   name: String,
-  image: String,
+  images: [ImageSchema],
   description: String,
   location: String,
   price: Number,
@@ -23,6 +36,9 @@ const skatespotSchema = new Schema({
 skatespotSchema.post('findOneAndDelete', async function(doc) {
   if(doc) {
     await Comment.deleteMany({ _id: { $in: doc.comments } });
+    for(let file of doc.images){
+      await cloudinary.uploader.destroy(file.filename);
+    }
   }
 });
 
