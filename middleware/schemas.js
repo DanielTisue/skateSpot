67ef0328d283adc8f505db177c7a13437a550414
DanielTisue <d.tisue@gmail.com>
2022-01-07 17:35:13 -0600

    Added html sanitation with Joi and additonal pckg.

--- middleware/schemas.js
+++ middleware/schemas.js
@@ -1,19 +1,42 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.skateSpotSchema = Joi.object({
     skatespot: Joi.object({
-        name: Joi.string().required(),
+        name: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
-        // image: Joi.array().min(1).max(4),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
 
 module.exports.commentSchema = Joi.object({
     comment: Joi.object({
-        text: Joi.string().required(),
+        text: Joi.string().required().escapeHTML(),
         rating: Joi.number().required().min(1).max(5)
     }).required()
 });
